0b1d4f4de17dab5950318a5909c8d39360a2d4d9
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-03-04 23:26:39 +0100

    Add a simple index page.

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ uri = URI.parse ENV["REDISTOGO_URL"]
 REDIS = Redis.new host: uri.host, port: uri.port, password: uri.password
 
 get '/' do
-  'hi.'
+  erb :index
 end
 
 get '/livepz/:id' do |id|
